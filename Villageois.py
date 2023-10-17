1dcad21f3325975af73c5a781df98dd2b7fda182
Wateli <148130694+Watelii@users.noreply.github.com>
2023-10-17 11:51:14 +0100

    Delete Villageois.py

--- Villageois.py
+++
@@ -1,7 +0,0 @@
-from Groupe import *
-
-class Villageois :
-    def __init__(self) :
-        self.type = 'Villageois'
-        self.reveal = 'Vous êtes villageois, démasquez les loups-garous et ne vous faîtes pas dévorer.'
-    
